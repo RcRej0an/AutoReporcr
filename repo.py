04fb9575b78c4923244f59761c3d0423a7a75cac
RcRej0an <103018983+RcRej0an@users.noreply.github.com>
2022-04-05 12:34:22 +0600

    Add files via upload

---
+++ repo.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Untitled document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="generator" content="pdftohtml 0.36"/>
+</head>
+<frameset cols="100,*">
+<frame name="links" src="repo_ind.html"/>
+<frame name="contents" src="repo-1.html"/>
+</frameset>
+</html>
